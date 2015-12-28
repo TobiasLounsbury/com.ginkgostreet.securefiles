5ee77eaad6e4f409d234a0715dd157c5ecc64577
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-12-28 13:56:25 -0800

    Added abstract methods to backend class

--- CRM/Securefiles/Backend.php
+++ CRM/Securefiles/Backend.php
@@ -1,6 +1,13 @@
 <?php
 
-class CRM_Securefiles_Backend {
+abstract class CRM_Securefiles_Backend {
+
+  abstract protected function uploadFile($file, $user = null);
+  abstract protected function downloadFile($file, $user = null);
+  abstract protected function listFiles($user = null);
+  abstract protected function fileMetadata($file, $user = null);
+
+  function cleanup() {}
 
 
 
