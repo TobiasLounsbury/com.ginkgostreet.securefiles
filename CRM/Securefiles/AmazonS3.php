200cbcfd1079ee39f89648b81f83217cc46f9591
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-12-28 13:57:15 -0800

    Added empty abstract methods to AmazonS3 adapter

--- CRM/Securefiles/AmazonS3.php
+++ CRM/Securefiles/AmazonS3.php
@@ -2,7 +2,9 @@
 
 class CRM_Securefiles_AmazonS3 extends CRM_Securefiles_Backend {
 
-
-
+  protected function uploadFile($file, $user = null) {}
+  protected function downloadFile($file, $user = null) {}
+  protected function listFiles($user = null) {}
+  protected function fileMetadata($file, $user = null) {}
 
 }
