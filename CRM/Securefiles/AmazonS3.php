43648b769f385c87a312c446b4be95364cbc4078
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-12-28 16:32:21 -0800

    Added new empty methods to AmazonS3 service

--- CRM/Securefiles/AmazonS3.php
+++ CRM/Securefiles/AmazonS3.php
@@ -4,7 +4,13 @@ class CRM_Securefiles_AmazonS3 extends CRM_Securefiles_Backend {
 
   protected function uploadFile($file, $user = null) {}
   protected function downloadFile($file, $user = null) {}
+  protected function deleteFile($file, $user = null) {}
   protected function listFiles($user = null) {}
   protected function fileMetadata($file, $user = null) {}
+
+  function buildSettingsForm(&$form) {}
+  function defaultSettings(&$defaults) {}
+  function saveSettings($values) {}
+  function validateSettings(&$form) { return true;}
 
 }
