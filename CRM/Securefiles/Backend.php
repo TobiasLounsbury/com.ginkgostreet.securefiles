be5a142cae28a10ab6e2fbfcf1c1b7eec100e835
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-12-28 16:31:56 -0800

    Added deleteFile abstract method

--- CRM/Securefiles/Backend.php
+++ CRM/Securefiles/Backend.php
@@ -4,6 +4,7 @@ abstract class CRM_Securefiles_Backend {
 
   abstract protected function uploadFile($file, $user = null);
   abstract protected function downloadFile($file, $user = null);
+  abstract protected function deleteFile($file, $user = null);
   abstract protected function listFiles($user = null);
   abstract protected function fileMetadata($file, $user = null);
 
