7f138afc3881f1572f9d1913254736f96ed7a778
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-01-04 10:44:59 -0800

    Updated view logic for amazon fields on settings page

--- js/securefiles_settings_amazon_s3.js
+++ js/securefiles_settings_amazon_s3.js
@@ -20,7 +20,18 @@ CRM.$(function ($) {
     }
   });
 
+  $("#securefiles_s3_use_sts").change(function(e) {
+    if($(this).is(":checked")) {
+      $(".securefiles_s3_sts_key-section").slideDown();
+      $(".securefiles_s3_sts_secret-section").slideDown();
+    } else {
+      $(".securefiles_s3_sts_key-section").slideUp();
+      $(".securefiles_s3_sts_secret-section").slideUp();
+    }
+  });
 
   $("#securefiles_s3_use_encryption").change();
+  $("#securefiles_s3_encryption_type").change();
+  $("#securefiles_s3_use_sts").change();
 
 });
